15f992d9226821d88289b08f2187be8032e06cc5
Elroi-thecreator <predeepsarc@gmail.com>
2026-08-23 15:39:59 +0530

    Update build.gradle.kts with new dependencies
    Added new dependencies for Compose UI graphics and tooling preview.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,6 +36,8 @@ dependencies {
     implementation("androidx.activity:activity-compose:1.9.1")
     implementation(platform("androidx.compose:compose-bom:2024.06.00"))
     implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-graphics")
+    implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     implementation("com.squareup.retrofit2:retrofit:2.11.0")
     implementation("com.squareup.retrofit2:converter-gson:2.11.0")
